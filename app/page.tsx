a469a1a2d5bae455d8f4e9582ff63302fc71c594
wowalswjd <alswjd5491@naver.com>
2024-11-22 19:38:14 +0900

    style: 불필요한 파일 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import styles from "@/app/page.module.css";
-
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return <div>Home</div>;
 }
