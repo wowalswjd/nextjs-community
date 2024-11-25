06cea8b7f3e014992c33fa09c48e2b5962ef3343
wowalswjd <alswjd5491@naver.com>
2024-11-26 03:06:29 +0900

    feat: 글 POST 요청 시 유저 정보 함께 저장하기

--- pages/api/post/create.ts
+++ pages/api/post/create.ts
@@ -1,6 +1,16 @@
 import type { NextApiHandler } from "next";
 import { postCollection } from "@/utils/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
 const handler: NextApiHandler = async (req, res) => {
+  let session = await getServerSession(req, res, authOptions);
+
+  if (session) {
+    req.body.author = session.user?.email;
+  }
+  console.log(req.body);
+
   if (req.method === "POST") {
     if (!req.body.title || !req.body.content) {
       return res.status(500).json({ error: "빈 형식이 있습니다." });
