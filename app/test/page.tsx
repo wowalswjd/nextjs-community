f5c3ca7b1aa1e4f60d780db31924a596b8185717
wowalswjd <alswjd5491@naver.com>
2024-11-26 00:45:49 +0900

    feat: test 페이지 제작

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function Page() {
+  let session = useSession();
+  if (session) {
+    console.log(session);
+  }
+}
