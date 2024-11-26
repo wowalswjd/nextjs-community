6123b026f743887c3f6932c1e40f87f61734dc2f
wowalswjd <alswjd5491@naver.com>
2024-11-26 12:54:00 +0900

    style: Header 코드 정리

--- app/Header.tsx
+++ app/Header.tsx
@@ -11,23 +11,21 @@ type HeaderProps = {
 
 export default function Header({ session }: HeaderProps) {
   return (
-    <div>
-      <div className="navbar">
-        <Link href="/" className="logo">
-          EFUB4 Forum
-        </Link>
-        <Link href="/list">List</Link>
-        <Link href="/write">Write</Link>
+    <div className="navbar">
+      <Link href="/" className="logo">
+        EFUB4 Forum
+      </Link>
+      <Link href="/list">List</Link>
+      <Link href="/write">Write</Link>
 
-        {session ? (
-          <span>
-            <span>{session?.user?.name && `${session.user.name}님 `}</span>
-            <LogoutBtn />
-          </span>
-        ) : (
-          <LoginBtn />
-        )}
-      </div>
+      {session ? (
+        <span>
+          <span>{session?.user?.name && `${session.user.name}님 `}</span>
+          <LogoutBtn />
+        </span>
+      ) : (
+        <LoginBtn />
+      )}
     </div>
   );
 }
